9c82f81312e62e4206c05a2c2f01cf505b1f46eb
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-05-16 23:02:13 +0200

    Añadida nueva escena
    Añadido script que permite seleccionar gameobjects, comienza a poderse
    rotar algo, hay un error la rotacion se reestablece a la inicial cuando
    pulsas algo, por algun motivo no rota bien sobre el eje z ya lo vemos

---
+++ Quaternion/Assets/scripts/ClicknDrag.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+
+var ray;
+var selectionColor : Color;
+private var gameobject : GameObject;
+private var lastColor : Color;
+private var mouseScreenPoint : Vector3;
+private var offset : Vector3;
+
+
+function Start(){
+    Cursor.visible = true;
+}
+
+function Update(){
+    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    var rayHit : RaycastHit;
+
+    if(Input.GetMouseButtonDown(0)){
+        Cursor.visible = false;
+        if(Physics.Raycast(ray, rayHit)){
+            gameobject = rayHit.collider.gameObject;
+            lastColor = gameobject.GetComponent.<Renderer>().material.color;
+            if(gameobject != null){
+                Debug.Log(gameobject.name);
+                gameobject.GetComponent.<Renderer>().material.color = selectionColor;
+                mouseScreenPoint = Input.mousePosition;
+            }
+        }
+    } else if(Input.GetMouseButton(0)){
+        var new_mouseScreenPoint = Input.mousePosition;
+        var rotate_angle = new_mouseScreenPoint - mouseScreenPoint;
+        Debug.Log(rotate_angle.y);
+
+        if(gameobject.name == "X"){
+            Debug.Log(gameobject.transform.rotation);
+            gameobject.transform.rotation = Quaternion.Euler(rotate_angle.y,0,90);
+        } 
+        else if(gameobject.name == "Y"){
+            Debug.Log(gameobject.transform.rotation);
+            gameobject.transform.rotation = Quaternion.Euler(0,rotate_angle.y,0);
+        } 
+        else if(gameobject.name == "Z"){
+            Debug.Log(gameobject.transform.rotation);
+            gameobject.transform.rotation = Quaternion.Euler(90,0,rotate_angle.y);
+
+        }
+    }
+    else if(Input.GetMouseButtonUp(0)) {
+        Cursor.visible = true;
+        gameobject.GetComponent.<Renderer>().material.color = lastColor;
+        gameobject = null;
+    }
+}
