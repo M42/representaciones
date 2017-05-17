5528912d8a67d053031620ba885486290bf08443
David Charte <fdavidcl@protonmail.com>
2017-05-17 20:20:58 +0200

    Actualiza animación unity

--- Quaternion/Assets/scripts/ClicknDrag.js
+++ Quaternion/Assets/scripts/ClicknDrag.js
@@ -15,52 +15,71 @@ var xRotation : Quaternion;
 var yRotation : Quaternion;
 var zRotation : Quaternion;
 
+private var rotx : float;
+private var roty : float;
+private var rotz : float;
+
+var speed : float = 2;
+
 function Start(){
-    Cursor.visible = true;
+  Cursor.visible = true;
+  xRotation = xRotor.transform.rotation;
+  yRotation = yRotor.transform.rotation;
+  zRotation = zRotor.transform.rotation;
 }
 
 function Update(){
-    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-    if(Input.GetMouseButtonDown(0)){
-        Cursor.visible = false;
-        if(Physics.Raycast(ray, rayHit)){
-            gameobject = rayHit.collider.gameObject;
-            lastColor = gameobject.GetComponent.<Renderer>().material.color;
-            if(gameobject != null){
-                Debug.Log(gameobject.name);
-                gameobject.GetComponent.<Renderer>().material.color = selectionColor;
-                mouseScreenPoint = Input.mousePosition;
-            }
-        }
-
-        xRotor.transform.rotation = xRotation;
-        yRotor.transform.rotation = yRotation;
-        zRotor.transform.rotation = zRotation;
-    } else if(Input.GetMouseButton(0)){
-        var new_mouseScreenPoint = Input.mousePosition;
-        var rotate_angle = new_mouseScreenPoint - mouseScreenPoint;
-        var foo = zRotor.transform.rotation.z;
-        //Debug.Log(rotate_angle.y);
-
-        if(gameobject.name == xRotor.name){
-            Debug.Log("X: "+xRotor.transform.rotation);
-            xRotor.transform.rotation = xRotation = Quaternion.Euler(rotate_angle.y,0,90);
-        } 
-        else if(gameobject.name == yRotor.name){
-            Debug.Log("Y: "+yRotor.transform.rotation);
-            yRotor.transform.rotation = yRotation = Quaternion.Euler(0,rotate_angle.y,0);
-        } 
-        else if(gameobject.name == zRotor.name){
-        	Debug.Log(foo);
-            Debug.Log("Z: "+zRotor.transform.rotation);
-            zRotor.transform.rotation = zRotation = Quaternion.Euler(90,0,0)*Quaternion.Euler(0,rotate_angle.y,0);
-
-        }
+  if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)){
+    Cursor.visible = false;
+    if(Physics.Raycast(ray, rayHit)){
+      gameobject = rayHit.collider.gameObject;
+      lastColor = gameobject.GetComponent.<Renderer>().material.color;
+      if(gameobject != null){
+        Debug.Log(gameobject.name);
+        gameobject.GetComponent.<Renderer>().material.color = selectionColor;
+        mouseScreenPoint = Input.mousePosition;
+      }
     }
-    else if(Input.GetMouseButtonUp(0)) {
-        Cursor.visible = true;
-        gameobject.GetComponent.<Renderer>().material.color = lastColor;
-        gameobject = null;
+    rotx = roty = rotz = 0;
+  } else if(Input.GetMouseButton(0) || Input.GetMouseButton(1)){
+    var new_mouseScreenPoint = Input.mousePosition;
+    var rotate_angle = new_mouseScreenPoint - mouseScreenPoint;
+    var foo = zRotor.transform.rotation.z;
+    var sign = Input.GetMouseButton(1) ? -1 : 1;
+    //Debug.Log(rotate_angle.y);
+
+    if (gameobject != null) {
+      if(gameobject.name == xRotor.name){
+        //Debug.Log("X: "+xRotor.transform.rotation);
+        rotx += sign * speed;
+        xRotor.transform.rotation = xRotation * Quaternion.Euler(0,rotx,0);
+      } 
+      else if(gameobject.name == yRotor.name){
+        //Debug.Log("Y: "+yRotor.transform.rotation);
+        roty += sign * speed;
+        yRotor.transform.rotation = yRotation * Quaternion.Euler(0,roty,0);
+      } 
+      else if(gameobject.name == zRotor.name){
+        //Debug.Log(foo);
+        //Debug.Log("Z: "+zRotor.transform.rotation);
+        rotz += sign * speed;
+        zRotor.transform.rotation = zRotation * Quaternion.Euler(0,rotz,0);
+
+      }
+      else {
+        Debug.Log(gameobject.transform.rotation);
+      }
     }
+  }
+  else if(Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1)) {
+    Cursor.visible = true;
+    gameobject.GetComponent.<Renderer>().material.color = lastColor;
+    gameobject = null;
+
+    zRotation = zRotor.transform.rotation;
+    xRotation = xRotor.transform.rotation;
+    yRotation = yRotor.transform.rotation;
+  }
 }
---
+++ Quaternion/Assets/scripts/Interpolation.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var t : float;
+var capsule : GameObject;
+var sphere : GameObject;
+
+private var initial : Quaternion;
+private var end : Quaternion;
+
+function Start () {
+  t = 0;
+  initial = capsule.transform.rotation;
+  end = new Quaternion(-0.5, 0.5, 0.5, 0.5);
+}
+
+function Update () {
+  if (Input.GetKey ("up")){
+    t += 0.1;
+    t = t > 1 ? 1 : t;
+    Debug.Log(t);
+    capsule.transform.rotation = Quaternion.Slerp(initial, end, t);
+  }
+}
