aefbd5bb85b3d9c059259afc6f141da2b8ff3949
David Charte <fdavidcl@protonmail.com>
2017-05-18 01:16:25 +0200

    Añade animación por slerp

--- Quaternion/Assets/scripts/Interpolation.js
+++ Quaternion/Assets/scripts/Interpolation.js
@@ -1,23 +1,48 @@
 ﻿#pragma strict
 
+var ray;
+private var rayHit : RaycastHit;
+
 var t : float;
 var capsule : GameObject;
 var sphere : GameObject;
+var clicked : GameObject;
 
 private var initial : Quaternion;
 private var end : Quaternion;
 
+private var enable_anim : boolean;
+
 function Start () {
   t = 0;
-  initial = capsule.transform.rotation;
+  enable_anim = false;
   end = new Quaternion(-0.5, 0.5, 0.5, 0.5);
 }
 
 function Update () {
-  if (Input.GetKey ("up")){
-    t += 0.1;
-    t = t > 1 ? 1 : t;
-    Debug.Log(t);
+  ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+  // detect a click on the sphere
+  if (Input.GetMouseButtonUp(0)) {
+    if (Physics.Raycast(ray, rayHit)) {
+      clicked = rayHit.collider.gameObject;
+
+      if (clicked == sphere) {
+        initial = capsule.transform.rotation;
+        enable_anim = true;
+      }
+    }
+  }
+  
+  if (enable_anim) {
+    // update animation
+    t += 0.025;
     capsule.transform.rotation = Quaternion.Slerp(initial, end, t);
+
+    // stop animation when finished
+    if (t >= 1) {
+      enable_anim = false;
+      t = 0;
+    }
   }
 }
